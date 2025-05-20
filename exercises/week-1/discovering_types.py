d78c7c625a5fb76f50e25189f78ad3d9362873b5
Andrew <andrewvelizcastro7@outlook.com>
2025-05-20 08:38:20 +0100

    Added code with the different types of data called discovering_types.py

---
+++ exercises/week-1/discovering_types.py
@@ -0,0 +1,9 @@
+type_string = "Hello, how are you?" # This is a string because it is between ""
+type_integer = 75 # This is an integer because it is a whole number
+type_boolean = True # Booleans can only be True or False. This is a True boolean
+type_float = 75.56 # This is a float because it has decimals
+
+print(type(type_string)) # Use type() to find the type of variable
+print(type(type_integer))
+print(type(type_boolean))
+print(type(type_float))
